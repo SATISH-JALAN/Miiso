f3ded0f0e32ea7e0522c850cb925ab1dc8ecd95b
Om <om.003780@snuindia.in>
2026-06-15 01:04:53 +0530

    feat: implement Hero section and integrate into Home page layout

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -2,21 +2,21 @@ import { StrictMode } from 'react';
 import { Hero } from './Hero';
 import { About } from './About';
 import { Features } from './Features';
-import { HowItWorks, ProblemStats, PermissionTrust, StoryProof, ComparisonTable, FooterCTA } from './Sections';
-import { Terminal } from './Terminal';
+import { HowItWorks, ProblemStats, PermissionTrust, StoryProof, FooterCTA } from './Sections';
+
 
 export function Home() {
   return (
     <div className="w-full">
       <Hero />
-      <Terminal />
+
       <About />
       <Features />
       <HowItWorks />
       <ProblemStats />
       <PermissionTrust />
       <StoryProof />
-      <ComparisonTable />
+
       <FooterCTA />
     </div>
   );
